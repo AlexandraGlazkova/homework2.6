e10acdac11e7a02b774b143d50f752ba01c8390c
GlakovaA <22109@bk.ru>
2022-09-26 12:09:30 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        System.out.println(nums);
+//        List<Integer> even = new ArrayList<Integer>();
+        for (int i = 0; i < nums.size(); i++) {
+            int x = nums.get(i);
+            if (x % 2 == 0) {
+                System.out.print(x+" ");
+            }else{
+                System.out.print(" ");
+            }
+
+        }
     }
 }
+
